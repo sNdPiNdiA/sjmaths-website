1eb4ca1f6d0aae08af00a25344e3a728fc4a35d2
sNdPiNdiA <110993402+sNdPiNdiA@users.noreply.github.com>
2026-01-17 04:41:01 +0530

    fix firebase cdn from project settings

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,15 +1,14 @@
-// assets/js/firebase-config.js
-import { initializeApp, getApps, getApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { initializeApp, getApps, getApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-firestore.js";
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyAoKGON0Q3NBLfnOuDPTPN_tGCqM-ed2M",
+  apiKey: "AIzaSyA0kGONQM03NBLfnOuDPTPN_tGCqM-ed2M",
   authDomain: "sjmaths-web.firebaseapp.com",
   projectId: "sjmaths-web",
-  storageBucket: "sjmaths-web.appspot.com",
+  storageBucket: "sjmaths-web.firebasestorage.app",
   messagingSenderId: "168858335686",
-  appId: "1:168858335686:web:9fa87028b7b71db7e1ac7",
+  appId: "1:168858335686:web:9f9a87028b7b71db7e1ac7",
   measurementId: "G-K326N2KJ2G"
 };
 
