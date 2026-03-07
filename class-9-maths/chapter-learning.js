fd1feccc649a38fb0f4548e5e7984a2a22d9908c
sNdPiNdiA <110993402+sNdPiNdiA@users.noreply.github.com>
2026-03-07 15:06:14 +0530

    Fix maths rendering: Added $ delimiters to JSONs and updated KaTeX config

--- class-9-maths/chapter-learning.js
+++ class-9-maths/chapter-learning.js
@@ -113,9 +113,11 @@
         if (window.renderMathInElement) {
             renderMathInElement(document.body, {
                 delimiters: [
+                    { left: '$$', right: '$$', display: true },
+                    { left: '$', right: '$', display: false },
                     { left: '\\(', right: '\\)', display: false },
-                    { left: '\\\\(', right: '\\\\)', display: false },
                     { left: '\\[', right: '\\]', display: true },
+                    { left: '\\\\(', right: '\\\\)', display: false },
                     { left: '\\\\[', right: '\\\\]', display: true }
                 ],
                 throwOnError: false
