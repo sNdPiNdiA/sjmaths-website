2eb01423754934d63a44c1a6ad34b9ee36ea0c67
sNdPiNdiA <110993402+sNdPiNdiA@users.noreply.github.com>
2026-05-10 11:12:54 +0530

    feat: modernize class 10 maths notes with animation engine and interactive quizzes for 2025-26 board exams

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,13 @@
-const CACHE_NAME = 'sjmaths-v1778350776';
+const CACHE_NAME = 'sjmaths-v1778391759';
 const ASSETS = [
     './',
     './index.html',
     './offline.html',
-    './assets/css/main.min.css?v=1778350776',
-    './assets/css/layout.min.css?v=1778350776',
-    './assets/css/component.min.css?v=1778350776',
-    './assets/css/improved-ui.min.css?v=1778350776',
-    './assets/vendor/fontawesome/css/all.min.css?v=1778350776',
+    './assets/css/main.min.css?v=1778391759',
+    './assets/css/layout.min.css?v=1778391759',
+    './assets/css/component.min.css?v=1778391759',
+    './assets/css/improved-ui.min.css?v=1778391759',
+    './assets/vendor/fontawesome/css/all.min.css?v=1778391759',
     './components/header.html',
     './components/footer.html'
 ];
