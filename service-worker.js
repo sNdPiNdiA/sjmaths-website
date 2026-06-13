7d3e8d93bf9cb07a191c0afbb5dfce08b4d7a7f1
Sandeep <sandeepjaiswal215@gmail.com>
2026-06-13 13:06:45 +0530

    Restore clean UPSC checkpoint and remove merge artifacts

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,13 @@
-const CACHE_NAME = 'sjmaths-v1781327047';
+const CACHE_NAME = 'sjmaths-v1781279412';
 const ASSETS = [
     './',
     './index.html',
     './offline.html',
-    './assets/css/main.min.css?v=1781327047',
-    './assets/css/layout.min.css?v=1781327047',
-    './assets/css/component.min.css?v=1781327047',
-    './assets/css/improved-ui.min.css?v=1781327047',
-    './assets/vendor/fontawesome/css/all.min.css?v=1781327047',
+    './assets/css/main.min.css?v=1781279412',
+    './assets/css/layout.min.css?v=1781279412',
+    './assets/css/component.min.css?v=1781279412',
+    './assets/css/improved-ui.min.css?v=1781279412',
+    './assets/vendor/fontawesome/css/all.min.css?v=1781279412',
     './components/header.html',
     './components/footer.html'
 ];
